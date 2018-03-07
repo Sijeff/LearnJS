2f0715259517a81301ecf201b9dc8c4a2a22981b
Simon Pellijeff <simonpellijeff@gmail.com>
2018-03-07 16:27:35 +0100

    Add background colors, change content

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,3 @@
 $(document).ready(function () {
-    var counter = getRandomInt(190000);
-
-
-    $("#counter").text("Visitors: " + counter);
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
+    
 });
